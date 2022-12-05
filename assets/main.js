e330916ba5dc9e69acca245a031bbba7116a7324
Tuan Nguyen <tuannase170164@fpt.edu.vn>
2022-12-05 22:38:01 +0700

    hello

--- assets/main.js
+++ assets/main.js
@@ -152,47 +152,15 @@ function sendBtn()
     //Hàm to(phần tử được chọn,thời gian di chuyển,
     //{hiệu ứng},”khoảng cách 2 chuyển động”)
     var btnText = document.querySelector('.btn p');
-    var timeLine = new TimelineMax({
-        paused: false
-    });    
     btn.addEventListener("click", () => {
-        // timeLine
-        //     .to(btnText, 0.6, {
-        //         opacity: 0
-        //     })
-        //     .to(btn, 0.8, {
-        //         height: 0.2,
-        //         opacity: 0.5,
-        //         boxShadow: "0px 0px 35px 7px #cd28fa",
-        //         delay: 0.25
-        //     })
-        //     .to(btn, 0.1, {
-        //         opacity: 0.5,
-        //         background: "#26ff92"
-        //     })
-        //     .to(btn, 0, {
-        //         width: 1,
-        //         delay: 0.2
-        //     })
-        //     .to(btn, 0.1, {
-        //         boxShadow: "0px 0px 100px 55px #B01E68",
-        //         y: 90,
-        //         height: 100,
-        //         delay: 0.23
-        //     })
-        //     .to(btn, 0.3, {
-        //         height: 1000,
-        //         y: -1500,
-        //         boxShadow: "0px 0px 85px 17px #B01E68",
-        //         delay: 0.2
-        //     })
-                content.style.display = 'block';
-                audio.play();
-                btn.style.animation = 'Fade 1s ease-in forwards'
-            setTimeout(() => {
-                formvalidate();
-            },38000)
-        });    
+        createRain();
+        content.style.display = 'block';
+        audio.play();
+        btn.style.animation = 'Fade 0.5s ease-in forwards'
+        setTimeout(() => {
+            formvalidate();
+        },35000)
+    });    
 }
 sendBtn();
 star()
